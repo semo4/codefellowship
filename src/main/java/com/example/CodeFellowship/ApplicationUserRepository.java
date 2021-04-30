edd268ad77e8b4afea8bed8a5fbcab588789c152
semo4 <fotboy1788@hotmail.com>
2021-05-01 01:13:42 +0300

    coorect lab 18 and 19

--- src/main/java/com/example/CodeFellowship/ApplicationUserRepository.java
+++ src/main/java/com/example/CodeFellowship/ApplicationUserRepository.java
@@ -1,7 +1,8 @@
 package com.example.CodeFellowship;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ApplicationUserRepository extends CrudRepository<ApplicationUser, Integer> {
+public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, Integer> {
     public ApplicationUser findByUsername(String userName);
 }
--- src/main/java/com/example/CodeFellowship/WebSecurityConfig.java
+++ src/main/java/com/example/CodeFellowship/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.CodeFellowship;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -45,5 +46,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutUrl("/perform_logout")
                 .deleteCookies("JSESSIONID");
 
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
